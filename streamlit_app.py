d373f3d902ce54f9c2f92f48c50bfb9c2abc7fda
alvarezdap <alvarezdap@gmail.com>
2022-05-20 15:10:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('Budgeting tool')
 ingreso = st.number_input('Earnings', 0, 10000)
 egreso = st.number_input('Spendings',0,10000)
 ahorro = st.number_input('Savings',0,100000)
-tiempo_fondo_emergencia = st.radio('Pick one', [3, 6, 10, 12])
+tiempo_fondo_emergencia = st.radio('Months for Emergency Fund Calculation', [3, 6, 10, 12])
 balance = ingreso - egreso
 fondo_emergencia = egreso*tiempo_fondo_emergencia
 inversion = ahorro - fondo_emergencia
